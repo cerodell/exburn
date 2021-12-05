1c324499a82f9413bdfd3405561ff5a9b49eb68b
cerodell <rodell@colorado.edu>
2021-12-05 10:53:18 -0800

    created latdiff version for paper with nadyas edits

--- scripts/aq-cross-animate-close.py
+++ scripts/aq-cross-animate-close.py
@@ -21,55 +21,68 @@ import matplotlib.pylab as pylab
 import matplotlib as mpl
 
 
-pm_ef = 10.400
-modelrun = "F6V51M08Z22"  #'F6V41' 'F6V81'
-ds = 25
-# levels = np.arange(0, 2000.0, 10)
+#################### Define Inputs #########################
+pm_ef = 10.400  ## black spruce emissions factor prichard_wildland_2020
+modelrun = "F6V51M08Z22"  ## choose model run F(fueltype)V(vertical levels)M(moisture precentage)Z(z grad stretch)
+dxy = 25  ## horizontal resolution
+## used to make plotting levels
 a = np.arange(1, 10)
 b = 10 ** np.arange(4)
 levels = (b[:, np.newaxis] * a).flatten()
-levels = levels[:-8]
-cmap = mpl.cm.cubehelix_r
-norm = mpl.colors.BoundaryNorm(levels, cmap.N, extend="both")
-
-var = "tr17_1"
-title = "Tracer"
-units = "PM2.5 " + r"($μg m^{-3}$)"
-cmap = "cubehelix_r"
-
-fireshape_path = str(data_dir) + "/all_units/mygeodata_merged"
+cmap = mpl.cm.cubehelix_r  ## define colormap
+norm = mpl.colors.BoundaryNorm(
+    levels, cmap.N, extend="both"
+)  ## normalize colormap with levels
+var = "tr17_1"  ## name variable to plot
+title = "Tracer"  ## give plot a title
+units = "PM2.5 " + r"($μg m^{-3}$)"  ## define units
+fireshape_path = (
+    str(data_dir) + "/all_units/mygeodata_merged"
+)  ## file dir to burn unit polygones
+## define save directory adn create if none existent
 save_dir = Path(str(save_dir) + f"/{modelrun}/")
 save_dir.mkdir(parents=True, exist_ok=True)
-south_north = slice(110, None, None)
-# west_east = 74
+## narrow domain bounds
+south_north = slice(110, 200, None)
 west_east = slice(30, 129, None)
-bottom_top = slice(0, None, None)
+bottom_top = slice(0, 22, None)
+###########################################################################
+
+
+## open wrf model output as xarray
 wrf_ds = xr.open_dataset(str(data_dir) + f"/{modelrun}/wrfout_d01_2019-05-11_17:49:11")
+## use wrf python to get model heights
 ncfile = Dataset(str(data_dir) + f"/{modelrun}/wrfout_d01_2019-05-11_17:49:11")
 height = wrf.getvar(ncfile, "height")
 height = height.isel(south_north=132, west_east=74, bottom_top=bottom_top)
-# height = np.round(height.values[:, 0, 0]).astype(int)
 
+## get heatflux for plotting
 hfx = wrf_ds["GRNQFX"]
 hfx = hfx.sel(
     south_north=south_north,
-    # west_east=west_east,
-    # Time=slice(0, 100),
+    Time=slice(0, 100),
 ).sum(dim="west_east")
 
+## slice and dice domain to be smaller
 wrf_ds = wrf_ds.sel(
     south_north=south_north,
     west_east=west_east,
     bottom_top=bottom_top,
-    # Time=slice(0, 100),
+    Time=slice(0, 100),
 )
+## crosswind intigrate smoke
 var_da = wrf_ds[var].sum(dim="west_east").to_dataset()
-sn = wrf_ds.south_north * 25
+## get array of north south in meters
+sn = wrf_ds.south_north * dxy
+## get time lenght for animating
 dimT = len(wrf_ds.XTIME)
 
-fig = plt.figure(figsize=(14, 5))
-# fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)  # top and bottom left
+
+## create a figure for animmation
+fig = plt.figure(figsize=(14, 3))
+## make a subplot in the fig
+ax = fig.add_subplot(1, 1, 1)
+## get first time index and plot
 ds = var_da.isel(Time=0)
 ax.set_title(f"{title} \n" + ds.XTIME.values.astype(str)[:-10], fontsize=18)
 contour = ax.contourf(
@@ -85,11 +98,8 @@ contour = ax.contourf(
 ax.set_ylabel("Vertical (m)", fontsize=14, labelpad=10)
 ax.set_xlabel("Horizontal (m)", fontsize=14, labelpad=10)
 ax.tick_params(axis="both", which="major", labelsize=12)
-y = np.array([130, 128, 132, 167, 144]) - 110
-y = y * 25
 colors_list = plt.rcParams["axes.prop_cycle"].by_key()["color"]
-
-# ax.scatter(y[2], 32, color=colors_list[2], edgecolor ='k', s = 200, zorder =10)
+ax.scatter(y[2], 32, color=colors_list[2], edgecolor="k", s=200, zorder=10)
 
 hfx_i = hfx.isel(Time=0)
 ax_hx = ax.twinx()
@@ -105,6 +115,7 @@ ax_hx.tick_params(axis="y", colors="tab:red")
 ax_hx.set_ylim(0, 400)
 ax_hx.tick_params(axis="both", which="major", labelsize=12)
 
+# add colorbar
 cbar = plt.colorbar(contour, ax=ax_hx, pad=0.1)
 cbar.ax.tick_params(labelsize=12)
 cbar.set_label(units, rotation=270, fontsize=14, labelpad=20)
@@ -112,6 +123,10 @@ cbar.set_label(units, rotation=270, fontsize=14, labelpad=20)
 
 
 def update_plot(i):
+    """
+    functions to animate
+    input: (i) is time index
+    """
     ax_hx.clear()
     global contour
     for c in contour.collections:
@@ -145,7 +160,10 @@ def update_plot(i):
     return contour
 
 
+## make that fig tight
 fig.tight_layout()
+## animate and store plot
 ani = animation.FuncAnimation(fig, update_plot, dimT, interval=3)
-ani.save(str(save_dir) + f"/{var}-cross-full.mp4", writer="ffmpeg", fps=10, dpi=250)
+## save animation and close
+ani.save(str(save_dir) + f"/{var}-cross.mp4", writer="ffmpeg", fps=10, dpi=250)
 plt.close()
